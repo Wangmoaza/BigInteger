cbf7105c16b286061979d51abaf19260c43ad07f
Wangmoaza <haeun0512@hanmail.net>
2016-03-18 20:57:07 +0900

    wrote evaluate method

--- src/BigInteger.java
+++ src/BigInteger.java
@@ -3,21 +3,26 @@ import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
  
- 
+ /*
+  * 해야될 것:
+  * equals, hashcode method 추가
+  * 
+  */
+
 public class BigInteger
 {
     public static final String QUIT_COMMAND = "quit";
     public static final String MSG_INVALID_INPUT = "입력이 잘못되었습니다."; 
-    public static final int MAX_RESULT_LEN = 200;
-    public static final int MAX_INPUT_LEN = 100;
+    public static final int MAX_RESULT_SIZE = 200;
+    public static final int MAX_INPUT_SIZE = 100;
     
     // fields
     private int sign = 0; // 1 if positive, -1 if negative
-    private int[] arr;
+    private int[] intArray; // question: int array로 해도 되는가?
     private int digit = 0;
     
     // implement this
-    public static final Pattern EXPRESSION_PATTERN = Pattern.compile(""); // 이건 뭘까
+    public static final Pattern EXPRESSION_PATTERN = Pattern.compile(""); // question: 이건 뭘까??
     
  
     public BigInteger(int i)
@@ -35,7 +40,6 @@ public class BigInteger
     	//
     }
  
-    
     
     public BigInteger add(BigInteger big)
     {
@@ -56,15 +60,49 @@ public class BigInteger
  
     static BigInteger evaluate(String input) throws IllegalArgumentException
     {
-        // implement here
-        // parse input, remove white space
-        // using regex is allowed
- 
-        // One possible implementation
-        // BigInteger num1 = new BigInteger(arg1);
-        // BigInteger num2 = new BigInteger(arg2);
-        // BigInteger result = num1.add(num2);
-        // return result;
+     	String spaceRemovedInput = input.replaceAll("\\s", ""); // remove white space
+        String patternStr = "(?<sign>[[+][-]]?)(?<num>[0-9]+)"
+    	Pattern pattern = pattern.compile(patternStr);
+        Matcher matcher = pattern.matcher(spaceRemovedInput);
+        String arg1 = "";
+        String arg2 = "";
+        int operatorIndex = 0;
+        
+        // parse input
+        if (matcher.find())
+        {
+        	arg1 = matcher.group();
+        	operatorIndex = matcher.end();
+        }
+        
+        if (matcher.find(operatorIndex + 1))
+        {
+        	arg2 = matcher.group();
+        }
+        
+        BigInteger bigNum1 = new BigInteger(arg1);
+        BigInteger bigNum2 = new BigInteger(arg2);
+
+        switch (spaceRemovedInput.charAt(operatorIndex))
+        {
+        	case '+' : 
+        		BigInteger result = bigNum1.add(bigNum2);
+        		break;
+        
+        	case '-' : 
+        		BigInteger result = bigNum1.subtract(bigNum2);
+        		break;
+        
+        	case '*' : 
+        		BigInteger result = bigNum1.multiply(bigNum2);
+        		break;
+        
+        	default:
+        		System.out.println("something wrong with operator input");
+        		break;
+        }
+        
+        return result;
     }
  
     public static void main(String[] args) throws Exception
